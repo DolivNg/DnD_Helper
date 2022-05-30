c10e4cefe7a0ea3a697a656683710e867db0e7bb
Doliv-PC <doliv1092000@gmail.com>
2022-05-30 18:14:30 +0300

    Rename first Fragment -> ToolListFragment add model View

--- app/src/main/java/com/dolivdev/helperdnd/ui/toollist/ToolListViewModel.kt
+++ app/src/main/java/com/dolivdev/helperdnd/ui/toollist/ToolListViewModel.kt
@@ -1,10 +1,10 @@
-package com.dolivdev.helperdnd.ui.gallery
+package com.dolivdev.helperdnd.ui.toollist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class ToolListViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
--- app/src/main/java/com/dolivdev/helperdnd/ui/toollist/ToolsListFragment.kt
+++ app/src/main/java/com/dolivdev/helperdnd/ui/toollist/ToolsListFragment.kt
@@ -1,4 +1,4 @@
-package com.dolivdev.helperdnd.ui.gallery
+package com.dolivdev.helperdnd.ui.toollist
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dolivdev.helperdnd.databinding.FragmentGalleryBinding
+
+import com.dolivdev.helperdnd.databinding.FragmentToollistBinding
 
 class ToolsListFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentToollistBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,15 +23,14 @@ class ToolsListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val toolListViewModel =
+            ViewModelProvider(this).get(ToolListViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentToollistBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        toolListViewModel.text.observe(viewLifecycleOwner) {
+
         }
         return root
     }
