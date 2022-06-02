3dd7fa716ce39e7e6ca23899fd9caf1ffbf874ea
Doliv-PC <doliv1092000@gmail.com>
2022-06-02 12:19:30 +0300

    add EntityToole

---
+++ app/src/main/java/com/dolivdev/helperdnd/domain/entity/TooleEntity.kt
@@ -0,0 +1,10 @@
+package com.dolivdev.helperdnd.domain.entity
+
+data class TooleEntity(
+    var NameToole : String,
+    var PriceToole : String,
+    var WeightToole : String,
+    var DescriptionToole : String,
+    var CategoryToole : String,
+    var VisibleToole : Boolean
+)
