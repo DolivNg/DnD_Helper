331e3b648762d9b52201635d9792771bf0ec0215
dev_m <doliv1092000@gmail.com>
2022-07-12 18:00:07 +0300

    Add toolListFragment.

--- app/src/main/java/com/dolivdev/helperdnd/presenter/toollist/ToolsListFragment.kt
+++ app/src/main/java/com/dolivdev/helperdnd/presenter/toollist/ToolsListFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dolivdev.helperdnd.databinding.FragmentToollistBinding
-import com.dolivdev.helperdnd.presenter.toollist.adapter.TooleTableAdapter
+import com.dolivdev.helperdnd.presenter.toollist.adapter.EquipmentRecycleAdapter
 
 
 class ToolsListFragment : Fragment() {
@@ -23,12 +23,12 @@ class ToolsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val toolListViewModel =
-            ViewModelProvider(this).get(ToolListViewModel::class.java)
+            ViewModelProvider(this)[ToolListViewModel::class.java]
 
         _binding = FragmentToollistBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        _binding!!.recyclerToole.adapter = TooleTableAdapter()
+        _binding!!.recyclerToole.adapter = EquipmentRecycleAdapter()
 
         toolListViewModel.getArrayEntity().observe(viewLifecycleOwner) {
             //_binding!!.recyclerToole.adapter.//.setTooleListArrayList(it)
